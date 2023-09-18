cfa9579eb3f429b89fa412292ce959ffee544f98
dgikonyo <denniskimani05@gmail.com>
2023-09-18 19:15:40 +0300

    added kyc page

--- app/pages/kyc/page.tsx
+++ app/pages/kyc/page.tsx
@@ -32,3 +32,42 @@ export default function Page() {
     </section>
   );
 }
+
+function step1() {
+  <section className="step1">
+    <div className="row">
+      <div className="split left">
+        <p>Step 1/4</p>
+        <p>Tell us more about your fundraiser</p>
+      </div>
+      <div className="split right"></div>
+    </div>
+  </section>
+}
+
+function step2() {
+  <section className="step1">
+    <div className="row">
+      <div className="split left"></div>
+      <div className="split right"></div>
+    </div>
+  </section>
+}
+
+function step3() {
+  <section className="step1">
+    <div className="row">
+      <div className="split left"></div>
+      <div className="split right"></div>
+    </div>
+  </section>
+}
+
+function step4() {
+  <section className="step1">
+    <div className="row">
+      <div className="split left"></div>
+      <div className="split right"></div>
+    </div>
+  </section>
+}
