f2f1254af85ea81f4b61b08fe3be096610f89d7c
dgikonyo <denniskimani05@gmail.com>
2023-09-21 21:23:04 +0300

    added redux

--- app/pages/kyc/page.tsx
+++ app/pages/kyc/page.tsx
@@ -3,6 +3,7 @@ import Continue from "../../components/continue";
 
 export default function Page() {
   return (
+    <form className="form">
       <div className="conainer-fluid">
         <div className="row">
           <div className="col-md-6 col-xs-12">
@@ -13,10 +14,6 @@ export default function Page() {
                 width={92}
                 height={30}
               ></Image>
-            </div>
-
-            <div className="kycSteps">
-              <p>Step 1/4</p>
             </div>
 
             <div className="kycHeading1">
@@ -30,19 +27,34 @@ export default function Page() {
 
           <div className="col-md-6 col-xs-12">
             <div className="form-floating mb-3">
-              <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+              <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com" />
               <label>Where are you located in Kenya?</label>
             </div>
 
             <div className="dropdown">
               <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1">
-                What best describes your fundraiser
+                Who are you fundraising for?
               </button>
               <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                 <li className="dropdown-item">Business venture</li>
                 <li className="dropdown-item">Wedding</li>
                 <li className="dropdown-item">Funeral</li>
               </ul>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input type="number" className="form-control" id="floatingInput" placeholder="name@example.com" />
+              <label>Your target amount</label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com" />
+              <label>Provide a title for the fundraiser</label>
+            </div>
+
+            <div className="form-floating mb-3">
+              <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+              <label>Tell us your story</label>
             </div>
 
             <div className="kycContinueBtn">
@@ -52,5 +64,6 @@ export default function Page() {
           </div>
         </div>
       </div>
+    </form>
   );
 }
